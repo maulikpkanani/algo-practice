636a70552a6fdd8e18d9c4b77fc90d1371878cf0
Maulik Kanani <maulikkanani1894@gmail.com>
2019-02-13 13:24:39 -0500

    anagram problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// First build the helper function to transform the given string into and CharMap
 
-module.exports = anagrams;
+function anagrams (stringA, stringB) {
+  // comparing both the strings strictly call the helper function
+  return cleanString(stringA) === cleanString(stringB)
+}
+
+// Helper func to remove the white space and all punctuations --> replace(/[^\w]/g, '') split sort join
+function cleanString (str) {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('')
+}
+
+module.exports = anagrams
+
+/// ////////////////////////////////
+// function anagrams (stringA, stringB) {
+//   // call char map inside the anagrams func to create charmap of stringA and stringB
+//   const aCharMap = buildCharMap(stringA)
+//   const bCharMap = buildCharMap(stringB)
+
+//   // check the length of the charMap obj and check its length and compare
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false
+//   }
+
+//   // loop through charmap and check if the values of the key are same
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false
+//     }
+//   }
+
+//   return true
+// }
+
+// let buildCharMap = str => {
+//   const charMap = {}
+
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
+//   return charMap
+// }
