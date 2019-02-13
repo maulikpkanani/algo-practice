8850e4d100feff1774073cc31c6d520727e519bf
Maulik Kanani <maulikkanani1894@gmail.com>
2019-02-13 16:57:08 -0500

    steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,52 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Recursion
+// set base step
+// set defaults
+// solve by calling function again
+function steps (n, row = 0, stair = '') {
+  // base case
+  if (n === row) {
+    return
+  }
+  // output the result
+  if (n === stair.length) {
+    console.log(stair)
 
-module.exports = steps;
+    // increase the row to continue the loop
+    return steps(n, row + 1)
+  }
+
+  // deciding step
+  const add = stair.length <= row ? '#' : ' '
+  steps(n, row, stair + add)
+}
+
+module.exports = steps
+
+//
+
+// function steps (n) {
+//   // think of a matrix rows and column
+//   // perfect technique to transform the code into the matrix when only one value is given
+//   for (let row = 0; row < n; row++) {
+//     let stair = ''
+//     debugger
+
+//     // for loop inside a for loop , this loop will run full with only the initial value of the outer loop
+//     for (let column = 0; column < n; column++) {
+//       // checking the conditions if the inner loop value is less than or equalto the value of the outerloop then you have to add the # .....
+//       if (column <= row) {
+//         stair += '#'
+//         // if not the case you have to add space
+//       } else {
+//         debugger
+//         stair += ' '
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
+
+// steps(3)
