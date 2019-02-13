32296f2d14c67a6b4f98f8401feb4660c362fff1
Maulik Kanani <maulikkanani1894@gmail.com>
2019-02-12 19:40:31 -0500

    finding max char in the string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,33 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// perfect technique to create the objectMap for any string relate q ;
 
-module.exports = maxChar;
+function maxChar (str) {
+  // we will start off with the initialization of objMap , max, and maxChar,
+  const objMap = {}
+  let max = 0
+  let maxChar = ''
+
+  // now use for of loop to loop over to the string and create over objMap
+  // using for of and if conditions to transform string into ogj :)
+  for (let char of str) {
+    if (objMap[char]) {
+      objMap[char]++
+    } else {
+      objMap[char] = 1
+    }
+  }
+
+  // now use for in loop to loop through an object
+  for (let key in objMap) {
+    // Good use of brain to output the answer
+    if (objMap[key] > max) {
+      max = objMap[key]
+      maxChar = key
+    }
+  }
+  return key
+}
+
+module.exports = maxChar
