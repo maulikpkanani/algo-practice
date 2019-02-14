da155e7304a147b6cf8973c173153d1004d4fe90
Maulik Kanani <maulikkanani1894@gmail.com>
2019-02-14 01:16:37 -0500

    vowels problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,48 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// good use of brain Math Math
+function pyramid (n) { 
+  // first find out the midpoint
+  const midpoint = Math.floor((2 * n - 1) / 2)
 
-module.exports = pyramid;
+  // loop through row
+  for (let row = 0; row < n; row++) {
+    let level = ''
+
+    // for columns they will be n*2-1
+    for (let column = 0; column < 2 * n - 1; column++) {
+      // tricky in deciding the range to put the # sign
+      if (midpoint - row <= column && midpoint + row >= row) {
+        level += '#'
+      } else {
+        level = ' '
+      }
+    }
+  }
+  console.log(level)
+}
+
+
+module.exports = pyramid
+
+// function pyramid (n) {
+//   // first find out the midpoint
+//   const midpoint = Math.floor((2 * n - 1) / 2)
+
+//   // loop through row
+//   for (let row = 0; row < n; row++) {
+//     let level = ''
+
+//     // for columns they will be n*2-1
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       // tricky in deciding the range to put the # sign
+//       if (midpoint - row <= column && midpoint + row >= row) {
+//         level += '#'
+//       } else {
+//         level = ' '
+//       }
+//     }
+//   }
+//   console.log(level)
+// }
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels (str) {
+  const matches = str.match(/[aeiou]/gi)
+    return matches? matches.length:0
+}
 
-module.exports = vowels;
+module.exports = vowels
+
+// function vowels (str) {
+
+//   //set the count to 0
+// let count = 0;
+// //create an upfront array with vowels
+// const checker = ['a','e','i','o','u']
+
+// //iterate through the string and check that if it includes the vowels of array
+// for(let char of str.toLowerCase()){
+//   if(checker.includes(char)){
+//     count++
+//   }
+// }
+// return count
+// }
